e40a146d67541aa8058d3e00ed11b39b5a1d87e8
yungwilly <william_reyes@dlsu.edu.ph>
2019-07-09 23:10:50 +0800

    added to main.js

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,49 @@
-const mainContainer = document.querySelector('.mainContainer');
-const dashboard = document.querySelector('#dashboard')
-
-mainContainer.addEventListener('submit', loggingOut)
-
-function loggingOut(e){
-    e.preventDefault();
-    window.location = "landingPage.html"
+window.onload = function(){
+    
+    
+    
+    $('#home').on('click',function(){
+        $('#div_table').show().hide(); 
+        $('#krustyslaves').show(); 
+        $('#div_home').show();
+        console.log('working');
+    });
+    
+    $('#tables').on('click',function(){
+        $('#div_home').show().hide(); 
+        $('#krustyslaves').show().hide(); 
+        $('#div_choose_file').show().hide();
+        
+        $('#div_table').show();
+        
+        console.log('working');
+    });
+    
+    $('#charts').on('click',function(){
+        $('#div_home').show().hide(); 
+        $('#krustyslaves').show().hide(); 
+        $('#div_table').show().hide();
+        $('#div_choose_file').show().hide();
+        
+        console.log('working');
+    });
+    
+    $('#upload').on('click',function(){
+        $('#div_home').show().hide(); 
+        $('#krustyslaves').show().hide(); 
+        $('#div_table').show().hide();
+        
+        $('#div_choose_file').show();
+        
+        console.log('working');
+    });
+    
+    $('#logout').on('click',function(){
+        window.location = "LogIn.html";
+        console.log('working');
+    });
+    
+    
+    
+    
 }
