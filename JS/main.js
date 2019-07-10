2af2b12ca7faa4259085fa5b81eb9f6f72cc0e30
yungwilly <william_reyes@dlsu.edu.ph>
2019-07-10 18:19:05 +0800

    added charts to landingpage

--- JS/main.js
+++ JS/main.js
@@ -1,31 +1,10 @@
 window.onload = function(){
     
-    
-    $("#file-chooser").change(function(event){
-        var uploadedFile = event.target.files[0]; 
-        console.log(uploadedFile.type);
-        if(uploadedFile.type != "application/json") { 
-        alert("File not read. Please upload a .json file!"); 
-        return false;
-        }else{
-        alert("File read successfully!");   
-            if (uploadedFile) {
-                var readFile = new FileReader();
-                readFile.onload = function(e) { 
-                var contents = e.target.result;
-                var json = JSON.parse(contents);
-                traverse(json,process);
-                $('#table').show();
-                $('.yes_data').show();
-                $('.no_data').show().hide();
-                };
-                readFile.readAsText(uploadedFile);
-                }else { 
-                console.log("Failed to load file");
-                }
-        }
-    });
-    
+    var chart = [];
+    var title = ""; 
+    var chartApp = document.getElementById("div_chart");
+    var num_charts = -1;
+
     $('#home').on('click',function(){
         $('#div_table').show().hide(); 
         $('#div_chart').show().hide(); 
@@ -33,7 +12,7 @@ window.onload = function(){
         
         $('#div_home').show();
         
-        console.log('working');
+        console.log('home_working');
     });
     
     $('#tables').on('click',function(){
@@ -43,7 +22,7 @@ window.onload = function(){
         
         $('#div_table').show();
         
-        console.log('working');
+        console.log('tables_working');
     });
     
     $('#charts').on('click',function(){
@@ -53,7 +32,7 @@ window.onload = function(){
         
         $('#div_chart').show();
         
-        console.log('working');
+        console.log('charts_working');
     });
     
     $('#upload').on('click',function(){
@@ -63,13 +42,41 @@ window.onload = function(){
         
         $('#div_upload').show();
         
-        console.log('working');
+        console.log('upload_working');
     });
     
     $('#logout').on('click',function(){
         window.location = "LogIn.html";
-        console.log('working');
+        console.log('logout_working');
     });
+    
+    
+    $("#file-chooser").change(function(event){
+        var uploadedFile = event.target.files[0]; 
+        console.log(uploadedFile.type);
+        if(uploadedFile.type != "application/json") { 
+        alert("Please upload a JSON file."); 
+        return false;
+        }else{
+            alert("JSON File uploaded!");   
+            if (uploadedFile) {
+                var readFile = new FileReader();
+                readFile.onload = function(e) { 
+                var contents = e.target.result;
+                var json = JSON.parse(contents);
+                traverse(json,process);
+                $('#table').show();
+                $('.show_data').show();
+                $('.no_data').show().hide();
+                };
+                readFile.readAsText(uploadedFile);
+                }else { 
+                console.log("JSON File failed to load");
+                }
+        }
+    });
+    
+    //METHODS
     
     function traverse(json,process) {
         for(var i in json) {
@@ -82,12 +89,16 @@ window.onload = function(){
     
     function process(key,value) {
         if (value == "[object Object]"){
-            addHeader(key); 
+            addTableHeader(key); 
             title = key.split("_").join(" ");
         }
         else{
                 if (typeof value !== "string" && document.getElementsByClassName(title).length === 0){
+                    create_chart(title);
+                    addData(chart[num_charts], key, value);
                     $('#table tr:last').remove();
+                }else if(document.getElementsByClassName(title).length !== 0){
+                    addData(chart[num_charts], key, value);
                 }else
                     addJSONToTable(key, value); 
         }
@@ -97,7 +108,7 @@ window.onload = function(){
         $('#table tr:last').append('<td>' + key.split("_").join(" ") + '<\/td> <td>' + value + '<\/td>');
     }
     
-    function addHeader(key){
+    function addTableHeader(key){
         $("#table").append(
           "<tr>" +
             "<th>" + key.split("_").join(" ") + ": </th>" +
@@ -106,6 +117,71 @@ window.onload = function(){
                    
     }
     
+    function addData(chart, label, data) {
+        chart.data.labels.push(label);
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data.push(data);
+        });
+
+        chart.update();
+    }
                 
+ 
+    function create_chart(key){
+        var new_chart = document.createElement('canvas');
+        new_chart.className = key;
+        chartApp.appendChild(new_chart);
+        new_chart.style.backgroundColor = "white";
+        num_charts++;
+        
+        chart[num_charts] = new Chart(new_chart,
+        {"type":"bar",
+        "data":{"labels":[],
+        "datasets":[{"label":key,
+        "data":[],
+        "fill":false,
+        backgroundColor: "#FFB6C1",
+        "borderWidth":1}]},
+        "options":{
+        responsive: false,
+        maintainAspectRatio: false,
+        "scales":{
+        "yAxes":[{
+        "ticks":{
+        "beginAtZero":true}}]}}});  
+                                            
+        new_chart.style.display = "inline-block";
+    }
+    
+    $("#chart_type").change(function () {
+        var ctype = this.value;
+                    
+        var num_of_charts = chart.length - 1;
+        while (num_of_charts >= 0){
+            var chart_data = chart[num_of_charts].data;
+            var chart_option = chart[num_of_charts].options;
+            var ctx = document.getElementsByClassName(chart[num_of_charts].data.datasets[0].label);
+            myChart = new Chart(ctx, {
+            type: ctype,
+            data: chart_data,
+            options: chart_option
+            });
+                
+            num_of_charts--;
+                       
+        }
+    });
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
